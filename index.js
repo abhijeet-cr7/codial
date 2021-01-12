4763b2141109bfa53cadfbf9c078161942e39c64
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2021-01-12 23:07:41 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const port = 8001;
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+app.use(express.static('./assests'));
+app.use(expressLayouts);
+//route k upar lena hai layout 
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+// use express router
+app.use('/', require('./routes'));
+// set up the view engine
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port, function(err){
     if(err){
         console.log(`Error in port ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const homeController = require('../controllers/home_controllers');
+// const usersController = require("../controllers/users_controllers");
+
+// router.get('/users', usersController.profile);
+
+console.log('Router Loaded');
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName, require('./routerfile));
+module.exports = router;
+
+//  can we talk on call??num? m ha se portal pr generate krti hu call okay?haan okay
