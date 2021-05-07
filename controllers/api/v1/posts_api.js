2e29c521922b014d56a863f01d720836f6540172
abhijeet <abhijeetdbz7@gmail.com>
2021-05-07 11:08:13 +0530

    added post in the post_api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,19 @@
-module.exports.index = function(req,res)
+const Post = require('../../../models/post');
+
+module.exports.index = async function(req,res)
 {
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate : {
+            path: 'user'
+        }
+    });
+
     return res.json(200, {
         message: "list of posts",
-        posts : []
+        posts : posts
     })
 }
